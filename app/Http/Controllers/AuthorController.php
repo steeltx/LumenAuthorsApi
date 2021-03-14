f3c993423ef51e1b2e916169bc556e090cb62d69
steeltx <realsteel.gnu@gmail.com>
2021-03-14 15:56:34 -0600

    Funciones de servicio autores

--- app/Http/Controllers/AuthorController.php
+++ app/Http/Controllers/AuthorController.php
@@ -2,9 +2,11 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Author;
 use App\Traits\ApiResponser;
-use Illuminate\Http\Client\Request;
-use Illuminate\Http\Request as HttpRequest;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Auth;
 
 class AuthorController extends Controller
 {
@@ -26,15 +28,24 @@ class AuthorController extends Controller
      */
     public function index()
     {
-
+        $authors = Author::all();
+        return $this->successResponse($authors);
     }
 
     /**
      * Ingresar autores
      */
-    public function store(HttpRequest $request)
+    public function store(Request $request)
     {
+        $rules = [
+            'name' => 'required|max:255',
+            'gender' => 'required|max:255|in:male,female',
+            'country' => 'required|max:255'
+        ];
+        $this->validate($request, $rules);
+        $author = Author::create($request->all());
 
+        return $this->successResponse($author,Response::HTTP_CREATED);
     }
 
     /**
@@ -42,7 +53,9 @@ class AuthorController extends Controller
      */
     public function show($author)
     {
+        $author = Author::findOrFail($author);
 
+        return $this->successResponse($author);
     }
 
     /**
@@ -50,13 +63,28 @@ class AuthorController extends Controller
      */
     public function update(Request $request, $author)
     {
+        $rules = [
+            'name' => 'max:255',
+            'gender' => 'max:255|in:male,female',
+            'country' => 'max:255'
+        ];
+        $this->validate($request, $rules);
+        $author = Author::findOrFail($author);
+        $author->fill($request->all());
+        if($author->isClean()){
+            return $this->errorResponse('Al menos un valor debe cambiar',Response::HTTP_UNPROCESSABLE_ENTITY);
+        }
 
+        return $this->successResponse($author);
     }
 
     /**
      * Eliminar un autor
      */
     public function destroy($author){
+        $author = Author::findOrFail($author);
+        $author->delete();
 
+        return $this->successResponse($author);
     }
 }
